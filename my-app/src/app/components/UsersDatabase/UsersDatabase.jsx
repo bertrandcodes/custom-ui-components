b47ef565559b4ad830004efd6deed431d394cb39
Bertrand Shao <bertrandshao@gmail.com>
2025-06-04 17:55:56 -0700

    add create fn

--- my-app/src/app/components/UsersDatabase/UsersDatabase.jsx
+++ my-app/src/app/components/UsersDatabase/UsersDatabase.jsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useRef, useState } from "react";
 import "./UsersDatabase.css";
 /*
 Components
@@ -21,25 +24,70 @@ Plan
 */
 
 const UsersDatabase = () => {
+  const [names, setNames] = useState({ 1: "Bertrand Shao", 2: "Joei Sumiller" });
+  const [searchVal, setSearchVal] = useState("");
+  const [selectedId, setSelectedId] = useState(null);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const ref = useRef(3);
+
+  const disableCreate = selectedId === null && firstName.length === 0 && lastName.length === 0;
+
+  const createUser = () => {
+    setNames((prevState) => ({
+      ...prevState,
+      [ref.current]: `${firstName} ${lastName}`,
+    }));
+    ref.current += 1;
+    setFirstName("");
+    setLastName("");
+  };
+
   return (
     <div className="users-database">
-      <input className="users-database--search" />
+      <input
+        className="users-database--search"
+        onChange={(e) => setSearchVal(e.target.value)}
+        value={searchVal}
+        placeholder="Search"
+      />
       <div className="users-database__data-table">
         <ul>
-          <li>example</li>
+          {Object.entries(names)
+            .filter(([_, name]) => name.includes(searchVal))
+            .map(([id, name]) => (
+              <li
+                key={id}
+                onClick={() => setSelectedId(id)}
+                className={`${selectedId === id ? "selected" : ""}`}
+              >
+                {name}
+              </li>
+            ))}
         </ul>
       </div>
       <div className="users-database__name-inputs">
         <label htmlFor="users-database__first-name">First Name</label>
-        <input id="users-database__first-name" />
+        <input
+          id="users-database__first-name"
+          onChange={(e) => setFirstName(e.target.value)}
+          value={firstName}
+        />
         <label htmlFor="users-database__last-name">Last Name</label>
-        <input id="users-database__last-name" />
+        <input
+          id="users-database__last-name"
+          onChange={(e) => setLastName(e.target.value)}
+          value={lastName}
+        />
       </div>
       <div className="users-database__buttons">
-        <button>Create</button>
+        <button disabled={disableCreate} onClick={createUser}>
+          Create
+        </button>
         <button>Update</button>
         <button>Delete</button>
-        <button>Cancel</button>
+        <button onClick={() => setSelectedId(null)}>Cancel</button>
       </div>
     </div>
   );
